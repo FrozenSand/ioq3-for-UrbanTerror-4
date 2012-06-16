44369ecd73c32f6eeb7b58a6a7b372a0c3637a4f
Charles Duprey <barbatos@urbanterror.info>
2012-06-16 16:37:52 +0200

    added missing header

--- code/sys/sys_unix.c
+++ code/sys/sys_unix.c
@@ -34,6 +34,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include <sys/time.h>
 #include <pwd.h>
 #include <libgen.h>
+#include <signal.h>
 
 qboolean stdinIsATTY;
 
