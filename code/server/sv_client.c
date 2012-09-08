4ab224cdcd5827735e9b66cd0a2893cbb752abb1
Gh0sT <redhight@hotmail.fr>
2012-09-08 22:49:51 +0200

    Added B3 silent control

--- code/server/sv_client.c
+++ code/server/sv_client.c
@@ -1341,12 +1341,33 @@ void SV_ExecuteClientCommand( client_t *cl, const char *s, qboolean clientOK ) {
 	int			dollarCount;
 	int			i;
 	char		*arg;
+	char		*p;
+        char		*pp;
+	char		c;
+	char		pbs[1024];
 	qboolean 	bProcessed = qfalse;
 	qboolean 	exploitDetected = qfalse;
 	
 	
 	
 	Cmd_TokenizeString( s );
+
+        if (clientOK) 
+
+        {
+	    if (Q_stricmp("say", Cmd_Argv(0)) == 0 || Q_stricmp("say_team", Cmd_Argv(0)) == 0) 
+          {
+               p = Cmd_Argv(1);
+               c = *p;
+               if (c == '*')
+               {     
+                     pp = Cmd_Args();
+                     sprintf( pbs, "tell %i \"%s\"\n", (int)(cl - svs.clients), pp);
+                     s = pbs;
+                     Cmd_TokenizeString( s ); 
+                } 
+            }
+         }
 
 	// see if it is a server level command
 	for (u=ucmds ; u->name ; u++) {
