a0bc96913307cb9ed3268bb0732ce9de4c35cbd1
Charles Duprey <barbatos@urbanterror.info>
2013-02-25 16:37:45 +0100

    Improve the /play command - by Ensiform and ztm

--- code/client/snd_main.c
+++ code/client/snd_main.c
@@ -307,25 +307,26 @@ S_Play_f
 */
 void S_Play_f( void ) {
 	int 		i;
+	int			c;
 	sfxHandle_t	h;
-	char		name[256];
 
 	if( !si.RegisterSound || !si.StartLocalSound ) {
 		return;
 	}
 
-	i = 1;
-	while ( i<Cmd_Argc() ) {
-		if ( !Q_strrchr(Cmd_Argv(i), '.') ) {
-			Com_sprintf( name, sizeof(name), "%s.wav", Cmd_Argv(1) );
-		} else {
-			Q_strncpyz( name, Cmd_Argv(i), sizeof(name) );
-		}
-		h = si.RegisterSound( name, qfalse );
+	c = Cmd_Argc();
+
+	if( c < 2 ) {
+		Com_Printf ("Usage: play <sound filename> [sound filename] [sound filename] ...\n");
+		return;
+	}
+
+	for( i = 1; i < c; i++ ) {
+		h = si.RegisterSound( Cmd_Argv(i), qfalse );
+
 		if( h ) {
 			si.StartLocalSound( h, CHAN_LOCAL_SOUND );
 		}
-		i++;
 	}
 }
 
