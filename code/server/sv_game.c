6b20a96c5b2f9e44cbe5b0eb64cd1e0b64ddcc87
Fenix <danielepantaleone@me.com>
2013-03-19 15:36:31 +0100

    Added missing break

--- code/server/sv_game.c
+++ code/server/sv_game.c
@@ -892,10 +892,11 @@ intptr_t SV_GameSystemCalls( intptr_t *args ) {
 	case TRAP_CEIL:
 		return FloatAsInt( ceil( VMF(1) ) );
 
-
 	default:
 		Com_Error( ERR_DROP, "Bad game system trap: %ld", (long int) args[0] );
+		break;
 	}
+
 	return -1;
 }
 
