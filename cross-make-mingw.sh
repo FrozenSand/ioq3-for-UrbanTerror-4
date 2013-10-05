34c15a5544327676984b82b5dd6b9fd406cafd1c
Tomasz Kruk <unhitpl@gmail.com>
2013-10-05 22:42:24 +0200

    Update cross-compile script for mingw

--- cross-make-mingw.sh
+++ cross-make-mingw.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-exec make PLATFORM=mingw32 CC=i586-mingw32msvc-gcc LD=i586-mingw32msvc-ld WINDRES=i686-w64-mingw32-windres $*
+exec make PLATFORM=mingw32 CC=i686-w64-mingw32-gcc LD=i686-w64-mingw32-ld WINDRES=i686-w64-mingw32-windres $*
 
