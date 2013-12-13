ebdf1047a813f7bc457b0cd3ba8e8b2bf4c12397
p5yc0runn3r <p5yc0runn3r@gmail.com>
2013-12-13 23:15:43 +0100

    dmaHD enabled by default
    dmaHD enabled by default in preparation for new 44Khz sounds and since
    fixes where made.

--- code/client/snd_dmahd.c
+++ code/client/snd_dmahd.c
@@ -1025,7 +1025,7 @@ dmaHD_Enabled
 qboolean dmaHD_Enabled(void) 
 {
 	if (dmaHD_Enable == NULL)
-		dmaHD_Enable = Cvar_Get("dmaHD_enable", "0", CVAR_ARCHIVE); //@Barbatos - turned off by default
+		dmaHD_Enable = Cvar_Get("dmaHD_enable", "1", CVAR_ARCHIVE); //@p5yc0runn3r- Turn on by default
 
 	return (dmaHD_Enable->integer);
 }
