FrozenSand/ioq3-for-UrbanTerror-4

commit 6b2e8db00df41b7f9e3d498424e54021c269d100

Converted % characters to US

code/client/cl_main.c
@@ -156,7 +156,24 @@ not have future usercmd_t executed before it is executed
 ======================
 */
 void CL_AddReliableCommand( const char *cmd ) {
-	int		index;
+	int    index, i;
+	char   realCommand[MAX_STRING_CHARS];
+
+	Cmd_TokenizeString(cmd);
+	Q_strncpyz(realCommand, cmd, sizeof(realCommand));
+
+	if (!Q_stricmp(Cmd_Argv(0), "say") ||
+		!Q_stricmp(Cmd_Argv(0), "say_team") ||
+		!Q_stricmp(Cmd_Argv(0), "ut_radio") ||
+		!Q_stricmp(Cmd_Argv(0), "tell") ||
+		!Q_stricmp(Cmd_Argv(0), "tell_target") ||
+		!Q_stricmp(Cmd_Argv(0), "tell_attacker")) {
+		for (i = 0; i < strlen(realCommand); i++) {
+			if (realCommand[i] == '%') {
+				realCommand[i] = 31;
+			}
+		}
+	}
 
 	// if we would be losing an old command that hasn't been acknowledged,
 	// we must drop the connection
@@ -165,7 +182,7 @@ void CL_AddReliableCommand( const char *cmd ) {
 	}
 	clc.reliableSequence++;
 	index = clc.reliableSequence & ( MAX_RELIABLE_COMMANDS - 1 );
-	Q_strncpyz( clc.reliableCommands[ index ], cmd, sizeof( clc.reliableCommands[ index ] ) );
+	Q_strncpyz( clc.reliableCommands[ index ], realCommand, sizeof( clc.reliableCommands[ index ] ) );
 }
 
 /*

commit 6d3e53df63fe60d3ba394d974c0dfea04e0a2625

Fixed bad server address bug introduced in 6b2e8db0

code/client/cl_main.c
@@ -1293,6 +1293,7 @@ void CL_Connect_f( void ) {
 	clc.serverMessage[0] = 0;
 
 	server = Cmd_Argv (1);
+	Q_strncpyz( cls.servername, server, sizeof(cls.servername) );
 
 	if ( com_sv_running->integer && !strcmp( server, "localhost" ) ) {
 		// if running a local server, kill it
@@ -1310,7 +1311,6 @@ void CL_Connect_f( void ) {
 	CL_FlushMemory( );
 	*/
 
-	Q_strncpyz( cls.servername, server, sizeof(cls.servername) );
 
 	if (!NET_StringToAdr( cls.servername, &clc.serverAddress) ) {
 		Com_Printf ("Bad server address\n");