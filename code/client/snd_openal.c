53e93db09a2e1295acbd11d40f6563e68001b502
Barbatos <barbatos@f1m.fr>
2016-01-30 22:27:02 +0100

    Removed the "missing sound" beep. Fixes FrozenSand/UrbanTerror4#246

--- code/client/snd_openal.c
+++ code/client/snd_openal.c
@@ -384,7 +384,7 @@ qboolean S_AL_BufferInit( void )
 	numSfx = 0;
 
 	// Load the default sound, and lock it
-	default_sfx = S_AL_BufferFind("sound/feedback/hit.wav");
+	default_sfx = S_AL_BufferFind("sound/null.wav");
 	S_AL_BufferUse(default_sfx);
 	knownSfx[default_sfx].isLocked = qtrue;
 
