7b8213b07f2fdfa9e685d4a215878b793bd28c1d
Daniele Pantaleone <dpantaleone@domotz.com>
2017-04-25 22:14:04 +0200

    Fixes for server status command
    1) Clear color codes from client name not to break the padding
    2) Use CNCT instead of CON for connecting clients (3rd party softwares, like B3, expect this value)

--- code/server/sv_ccmds.c
+++ code/server/sv_ccmds.c
@@ -1181,70 +1181,71 @@ static int SV_Strlen( const char *str ) {
 SV_Status_f
 ================
 */
-static void SV_Status_f( void ) {
-	int			i, j, l;
-	client_t	*cl;
-	playerState_t	*ps;
-	const char		*s;
-	int			ping;
+static void SV_Status_f(void) {
 
-	// make sure server is running
-	if ( !com_sv_running->integer ) {
-		Com_Printf( "Server is not running.\n" );
+	int           i, j, l;
+	client_t      *cl;
+	playerState_t *ps;
+	const char    *s;
+	int           ping;
+	char          name[MAX_NAME_LENGTH];
+
+	if (!com_sv_running->integer) {
+		Com_Printf("Server is not running.\n");
 		return;
 	}
 
-	Com_Printf ("map: %s\n", sv_mapname->string );
+	Com_Printf("map: %s\n", sv_mapname->string);
+	Com_Printf("num score ping name            lastmsg address               qport rate \n");
+	Com_Printf("--- ----- ---- --------------- ------- --------------------- ----- -----\n");
 
-	Com_Printf ("cl score ping name            address                                 rate \n");
-	Com_Printf ("-- ----- ---- --------------- --------------------------------------- -----\n");
-	for (i=0,cl=svs.clients ; i < sv_maxclients->integer ; i++,cl++)
-	{
-		if (!cl->state)
+	for (i = 0, cl = svs.clients; i < sv_maxclients->integer; i++, cl++) {
+
+		if (!cl->state) {
 			continue;
-		Com_Printf ("%2i ", i);
-		ps = SV_GameClientNum( i );
-		Com_Printf ("%5i ", ps->persistant[PERS_SCORE]);
-
-		if (cl->state == CS_CONNECTED)
-			Com_Printf ("CON ");
-		else if (cl->state == CS_ZOMBIE)
-			Com_Printf ("ZMB ");
-		else
-		{
-			ping = cl->ping < 9999 ? cl->ping : 9999;
-			Com_Printf ("%4i ", ping);
 		}
 
-		Com_Printf ("%s", cl->name);
-		
-		l = 16 - SV_Strlen(cl->name);
-		j = 0;
-		
-		do
-		{
-			Com_Printf (" ");
-			j++;
-		} while(j < l);
+		Com_Printf("%3i ", i);
+		ps = SV_GameClientNum(i);
+		Com_Printf("%5i ", ps->persistant[PERS_SCORE]);
 
+		if (cl->state == CS_CONNECTED) {
+			Com_Printf("CNCT ");
+		} else if (cl->state == CS_ZOMBIE) {
+			Com_Printf("ZMBI ");
+		} else {
+			ping = cl->ping < 9999 ? cl->ping : 9999;
+			Com_Printf("%4i ", ping);
+		}
+
+		// Remove color codes not to break the padding.
+		Q_strncpyz(name, cl->name, sizeof(name));
+		Q_CleanStr(name);
+
+		Com_Printf("%s", name);
 
 		// TTimo adding a ^7 to reset the color
-		s = NET_AdrToString( cl->netchan.remoteAddress );
-		Com_Printf ("^7%s", s);
-		l = 39 - strlen(s);
-		j = 0;
-		
-		do
-		{
+		Com_Printf("^7");
+		l = (int) (16 - strlen(name));
+		for (j = 0; j < l; j++) {
 			Com_Printf(" ");
-			j++;
-		} while(j < l);
-		
-		Com_Printf (" %5i", cl->rate);
+		}
 
-		Com_Printf ("\n");
+		Com_Printf("%7i ", svs.time - cl->lastPacketTime);
+		s = NET_AdrToString(cl->netchan.remoteAddress);
+		Com_Printf("%s", s);
+		l = (int) (22 - strlen(s));
+		for (j = 0; j < l; j++) {
+			Com_Printf(" ");
+		}
+
+		Com_Printf("%5i", cl->netchan.qport);
+		Com_Printf(" %5i", cl->rate);
+		Com_Printf("\n");
 	}
-	Com_Printf ("\n");
+
+	Com_Printf("\n");
+
 }
 
 /*
