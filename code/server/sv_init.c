FrozenSand/ioq3-for-UrbanTerror-4

commit 212af5639692689d0e56fdab6960c3da3aeec8af

Added support for urt compressed pure list

code/server/sv_init.c
@@ -392,6 +392,191 @@ static void SV_TouchCGame(void) {
 
 /*
 ================
+TextEncode6Bit
+
+Encodes text using 6 bit alphabet for map names
+================
+*/
+
+int TextEncode6Bit(const char *nam, unsigned char *buf, int blen)
+{
+	static int char2val[256] = { // character mapping table
+			0,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+			1,2,-1,3,-1,4,5,6,7,8,-1,9,10,11,12,-1,13,14,15,16,17,18,19,20,21,22,-1,23,-1,24,-1,-1,25,-1,-1,
+			-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,26,-1,27,28,29,30,31,32,
+			33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,-1,58,59,-1,-1,-1,-1,
+			-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+			-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+			-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+			-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+	};
+	int ol;
+	static char tmp[BIG_INFO_STRING * 4];
+	char *tp = tmp+1;
+
+	tmp[0]=' '; // add space at start to compress first name using " ut4_"
+	while(*nam)  *(tp++) = tolower(*(nam++));
+
+	tp = tmp;
+	ol = 0;
+	while(1) { // Use 4 values for most common words (based on analysis of 5000+ map filenames)
+		int x;
+		char ch = *tp;
+		if (ch==' ' && tp[1]=='u' && tp[2]=='t' && tp[3]=='4' && tp[4]=='_') { // " ut4_"
+			x = 60; tp+=5;
+		} else
+		if (ch==' ' && tp[1]=='u' && tp[2]=='t' && tp[3]=='_') { // " ut_"
+			x = 61; tp+=4;
+		} else
+		if (ch=='_' && tp[1]=='b' && tp[2]=='e' && tp[3]=='t' && tp[4]=='a') { //"_beta"
+			x = 62; tp+=5;
+		} else
+		if (ch=='j' && tp[1]=='u' && tp[2]=='m' && tp[3]=='p' && tp[4]=='s') { //"jumps"
+			x = 63; tp+=5;
+		} else {
+			x = char2val[(unsigned int)ch];
+			tp++;
+		}
+		if (x == -1) {
+			Com_Printf("Invalid character in pk3 file name: '%c'\n",ch);
+			x = char2val['~']; // replace with ~ character to not break everythign else
+		}
+		buf[ol++] = x;
+		if (ol == blen) {
+			Com_Printf("TextEncode6Bit: target buffer overflow!\n");
+			buf[ol - 1] = 0;
+			return ol;
+		}
+		if (!ch)
+			return ol;
+	}
+}
+
+/*
+================
+SV_MakeCompressedPureList
+
+Fills the last N configstrings with compressed pure list
+================
+*/
+void SV_MakeCompressedPureList() {
+
+	unsigned char buf[PURE_COMPRESS_BUFFER];
+	char tmp[1025];
+	int i, l, bl, sh, shl, ol, csnr;
+	const char *nam;
+	msg_t msg = {qfalse};
+	const char *err_chunk = "Too many pk3 files (compressed data doesn't fit into available space)";
+
+	// Get raw checksums
+	bl = FS_LoadedPakChecksumsBlob(buf+2,sizeof(buf)-2);
+	if (!bl) Com_Error(ERR_DROP,"Too many pk3 files (size checksums > buffer)");
+	// Com_Printf("CRC SIZE(%d)\n",bl);
+
+	// Add number of files at start
+	l = bl / 4; // num files
+	buf[0] = (l) & 0xFF;
+	buf[1] = (l >> 8) & 0xFF;
+
+	// Get pak names
+	nam = FS_LoadedPakNames();
+#if 1
+	msg.cursize = 2+bl;
+	msg.cursize += TextEncode6Bit(nam,buf + msg.cursize, sizeof(buf) - msg.cursize);
+#else
+	msg.cursize = 2+bl+strlen(nam)+1;
+ if (msg.cursize>sizeof(buf)) Com_Error(ERR_DROP,"Too many pk3 files (no space in buffer for names, need %d bytes)",msg.cursize-sizeof(buf));
+ strcpy((char*)buf+2+bl,nam);
+#endif
+	Com_Printf("Pure filelist (%d files) size: %d\n",l,msg.cursize);
+
+	// fprintf(stderr,"FNAMES: %s\n",nam);
+	// Com_Printf("BUF(%d): ",msg.cursize);
+	// for(i=0;i<msg.cursize;i++) Com_Printf("%02X",buf[i]);
+	// Com_Printf("\n");
+
+	// Huffman compress names
+	msg.maxsize = sizeof(buf);
+	msg.data = buf;
+	Huff_Compress(&msg,2+bl);
+	bl = msg.cursize;
+
+	// fprintf(stderr,"COMPRESSED(%d): ",msg.cursize);
+	// for(i=0;i<msg.cursize;i++) fprintf(stderr,"%02X",buf[i]);
+	// fprintf(stderr,"\n");
+
+	Com_Printf("Pure filelist compressed size: %d\n",bl);
+
+	// Do 8bit to 7bit encoding, escaping \0 % " using @ char and original value + 1
+	csnr = 0;
+	sh = 0;
+	shl = 0;
+	ol = 0;
+	for(i = 0; i < bl; i++) {
+		sh = (sh << 8)|buf[i];
+		shl += 8;
+		//Com_Printf("IN:%02X\n",buf[i]);
+		while(shl >= 7) {
+			shl -= 7;
+			int v = (sh >> shl) & 127;
+			if (v == 0 || v=='"' || v=='%' || v=='@') {
+				tmp[ol++] = '@';
+				//Com_Printf("OUT:%02X\n",tmp[ol-1]);
+				if (ol == sizeof(tmp)-1) {
+					tmp[ol] = 0;
+					if (csnr == PURE_COMPRESS_NUMCS)
+						Com_Error(ERR_DROP, "%s", err_chunk);
+					SV_SetConfigstring(MAX_CONFIGSTRINGS - PURE_COMPRESS_NUMCS + csnr, tmp);
+					csnr++;
+					ol = 0;
+				}
+				tmp[ol++] = v + 1;
+			} else {
+				tmp[ol++] = v;
+			}
+			//Com_Printf("OUT:%02X\n",tmp[ol-1]);
+			if (ol == sizeof(tmp) - 1) {
+				tmp[ol] = 0;
+				if (csnr == PURE_COMPRESS_NUMCS)
+					Com_Error(ERR_DROP, "%s", err_chunk);
+				SV_SetConfigstring( MAX_CONFIGSTRINGS-PURE_COMPRESS_NUMCS+csnr, tmp);
+				csnr++;
+				ol=0;
+			}
+		}
+	}
+	// Com_Printf("[shl=%d sh=%X]\n",shl,sh);
+
+	// Finish remaining bits if any
+	if (shl > 0) {
+		int v = (sh << (7 - shl)) & 127;
+		if (v == 0 || v == '"' || v == '%' || v == '@') {
+			tmp[ol++] = '@';
+			if (ol == sizeof(tmp)-1) {
+				tmp[ol] = 0;
+				if (csnr == PURE_COMPRESS_NUMCS)
+					Com_Error(ERR_DROP, "%s", err_chunk);
+				SV_SetConfigstring(MAX_CONFIGSTRINGS - PURE_COMPRESS_NUMCS + csnr, tmp);
+				csnr++;
+				ol = 0;
+			}
+			tmp[ol++] = v+1;
+		} else {
+			tmp[ol++] = v;
+		}
+		//Com_Printf("OUT:%02X\n",tmp[ol-1]);
+	}
+	if (ol) {
+		if (csnr == PURE_COMPRESS_NUMCS)
+			Com_Error(ERR_DROP, "%s", err_chunk);
+		tmp[ol] = 0;
+		SV_SetConfigstring(MAX_CONFIGSTRINGS - PURE_COMPRESS_NUMCS + csnr, tmp);
+	}
+	Com_Printf("Using %d configstrings to store pure filelist (encoded using %d characters)\n", csnr + 1, ol + csnr * 1024);
+}
+
+/*
+================
 SV_SpawnServer
 
 Change the server to a new map, taking all connected
@@ -576,7 +761,21 @@ void SV_SpawnServer( char *server, qboolean killBots ) {
 
 	if ( sv_pure->integer ) {
 		// if a dedicated pure server we need to touch the cgame because it could be in a
-		// seperate pk3 file and the client will need to load the latest cgame.qvm
+		// separate pk3 file and the client will need to load the latest cgame.qvm
+		if (sv_newpurelist->integer) {
+			Cvar_Set( "sv_paks", "*" );
+			Cvar_Set( "sv_pakNames", "*" );
+			SV_MakeCompressedPureList();
+		} else {
+			p = FS_LoadedPakChecksums();
+			Cvar_Set( "sv_paks", p );
+			if (!p[0]) {
+				Com_Printf( "WARNING: sv_pure set but no PK3 files loaded\n" );
+			}
+			p = FS_LoadedPakNames();
+			Cvar_Set( "sv_pakNames", p );
+		}
+
 		if ( com_dedicated->integer ) {
 			SV_TouchCGame();
 		}
@@ -589,15 +788,6 @@ void SV_SpawnServer( char *server, qboolean killBots ) {
 			FS_SetExtraPure(server, sv_extraPaks->string);
 		}
 
-		// the server sends these to the clients so they will only
-		// load pk3s also loaded at the server
-		p = FS_LoadedPakChecksums();
-		Cvar_Set( "sv_paks", p );
-		if (strlen(p) == 0) {
-			Com_Printf( "WARNING: sv_pure set but no PK3 files loaded\n" );
-		}
-		p = FS_LoadedPakNames();
-		Cvar_Set( "sv_pakNames", p );
 	}
 	else {
 		Cvar_Set( "sv_paks", "" );
@@ -612,6 +802,29 @@ void SV_SpawnServer( char *server, qboolean killBots ) {
 
 	// save systeminfo and serverinfo strings
 	Q_strncpyz( systemInfo, Cvar_InfoString_Big( CVAR_SYSTEMINFO ), sizeof( systemInfo ) );
+
+	{
+		const char *t,*tt;
+		int l,l2;
+
+		t = Info_ValueForKey( systemInfo, "sv_paks" );
+		l = 0;
+		if (t) {
+			tt = t;
+			while(*tt) { if (*tt==' ') l++; tt++; }
+		}
+
+		t = Info_ValueForKey( systemInfo, "sv_pakNames" );
+		l2 = 0;
+		if (t) {
+			tt = t;
+			while(*tt) { if (*tt==' ') l2++; tt++; }
+		}
+		if (l != l2) {
+			Com_Printf( "WARNING: Pure pak file list inconsistency (%d checksums, %d file names)\n",l,l2 );
+		}
+	}
+
 	cvar_modifiedFlags &= ~CVAR_SYSTEMINFO;
 	SV_SetConfigstring( CS_SYSTEMINFO, systemInfo );
 
@@ -669,6 +882,7 @@ void SV_Init (void)
 	sv_maxPing = Cvar_Get ("sv_maxPing", "0", CVAR_ARCHIVE | CVAR_SERVERINFO );
 	sv_floodProtect = Cvar_Get ("sv_floodProtect", "2", CVAR_ARCHIVE | CVAR_SERVERINFO );
 	sv_clientsPerIp = Cvar_Get ("sv_clientsPerIp", "0", CVAR_ARCHIVE | CVAR_SERVERINFO );
+	sv_newpurelist = Cvar_Get ("sv_newpurelist", "0", CVAR_ARCHIVE );
 
 	// systeminfo
 	Cvar_Get ("sv_cheats", "1", CVAR_SYSTEMINFO | CVAR_ROM );

code/client/cl_parse.c
@@ -224,7 +224,7 @@ void CL_ParseSnapshot( msg_t *msg ) {
 
 	// if we were just unpaused, we can only *now* really let the
 	// change come into effect or the client hangs.
-	cl_paused->modified = 0;
+	cl_paused->modified = qfalse;
 
 	newSnap.messageNum = clc.serverMessageSequence;
 
@@ -331,6 +331,189 @@ int cl_connectedToPureServer;
 int cl_connectedToCheatServer;
 
 /*
+================
+CL_TextDecode6Bit
+
+Decode pk3 file names encoded usign 6 bit alphabet
+================
+*/
+
+void CL_TextDecode6Bit(unsigned char *buf, int blen, char *out, int olen) {
+
+	static char val2char[] = "\x00 !#%&'()+,-.0123456789;=@[]^_`abcdefghijklmnopqrstuvwxyz{}~";
+	int i;
+	int op = -1; // encode added space at start
+
+	for(i=0;i<blen;i++) {
+		int v = buf[i];
+		if (v<60) {
+			v = val2char[v];
+			if (op>=0) out[op] = v;
+			op++;
+			if (!v) return; // null terminated, we are done
+		} else {
+			switch (v-60) {
+				case 0:
+					if (op+5>olen) {
+						buf[op]=0;
+						Com_Printf("CL_TextDecode6Bit: target buffer overflow!\n");
+						return;
+					}
+					if (op>=0) out[op+0] = ' ';
+					out[op+1] = 'u';
+					out[op+2] = 't';
+					out[op+3] = '4';
+					out[op+4] = '_';
+					op+=5;
+					break;
+				case 1:
+					if (op+4>olen) {
+						buf[op]=0;
+						Com_Printf("CL_TextDecode6Bit: target buffer overflow!\n");
+						return;
+					}
+					if (op>=0) out[op+0] = ' ';
+					out[op+1] = 'u';
+					out[op+2] = 't';
+					out[op+3] = '_';
+					op+=4;
+					break;
+				case 2:
+					if (op+5>olen) {
+						buf[op]=0;
+						Com_Printf("CL_TextDecode6Bit: target buffer overflow!\n");
+						return;
+					}
+					if (op>=0) out[op+0] = '_';
+					out[op+1] = 'b';
+					out[op+2] = 'e';
+					out[op+3] = 't';
+					out[op+4] = 'a';
+					op+=5;
+					break;
+				case 3:
+					if (op+5>olen) {
+						buf[op]=0;
+						Com_Printf("CL_TextDecode6Bit: target buffer overflow!\n");
+						return;
+					}
+					if (op>=0) out[op+0] = 'j';
+					out[op+1] = 'u';
+					out[op+2] = 'm';
+					out[op+3] = 'p';
+					out[op+4] = 's';
+					op+=5;
+					break;
+				default:
+					break;
+			}
+		}
+		if (op==olen) {
+			break;
+		}
+	}
+}
+
+
+/*
+================
+CL_ParseCompressedPureList
+
+Decode, decompress and set pure file list from compressed data
+================
+*/
+void CL_ParseCompressedPureList() {
+
+	int i, esc, sh, shc, bl, l;
+	static unsigned char buf[PURE_COMPRESS_BUFFER];
+	static char sums[16384];
+	static char names[BIG_INFO_STRING * 4];
+	msg_t msg = {qfalse};
+
+	// Decode 7 bit encoding into 8 bit buffer
+	esc = 0;
+	shc = 0;
+	sh = 0;
+	bl = 0;
+	for(i = 0;(i < 8) && (bl < sizeof(buf)); i++) {
+		char *s = cl.gameState.stringData + cl.gameState.stringOffsets[MAX_CONFIGSTRINGS - PURE_COMPRESS_NUMCS + i];
+		while (*s) {
+			int v = *(s++);
+			if (esc) {
+				v--;
+				esc = 0;
+			} else {
+				if (v == '@') {
+					esc = 1;
+					continue;
+				}
+			}
+			sh = (sh << 7) | v;
+			shc += 7;
+			if (shc >= 8) {
+				shc -= 8;
+				v = (sh >> shc) & 0xFF;
+				buf[bl++] = v;
+				//fprintf(stderr,"%02X",v);
+			}
+		}
+	}
+	//fprintf(stderr,"\n");
+
+	Com_Printf("Pure filelist compressed size: %d\n", bl);
+
+	// Decompress huffman encoded filenames
+	msg.maxsize = sizeof(buf);
+	msg.cursize = bl;
+	msg.data = buf;
+	l = (buf[0] | (buf[1] << 8)) * 4;
+	if (l > msg.maxsize)
+		Com_Error(ERR_DROP, "Malformed compressed pure filelist");
+	Huff_Decompress(&msg, 2 + l);
+
+	Com_Printf("Pure filelist (%d files) decompressed size: %d\n", l / 4, bl);
+
+	// fprintf(stderr,"DECOMPRESSED(%d): ",msg.cursize);
+	// for(i=0;i<msg.cursize;i++) fprintf(stderr,"%02X",buf[i]);
+	// fprintf(stderr,"\n");
+
+	// create checksums string
+	sums[0]=0;
+	sh=0;
+	for(i=0;i<l;i+=4) {
+		char tmp[16];
+		int n;
+		unsigned int chs = buf[2+i+0]|(buf[2+i+1]<<8)|(buf[2+i+2]<<16)|(buf[2+i+3]<<24);
+		n = sprintf(tmp,"%i ",chs); // Can't use Com_sprintf, it doesn't return anything
+		if (sh + n + 1 >= sizeof(sums)) {
+			Com_Error(ERR_DROP,"Checksum buffer overflow");
+			break;
+		}
+		strcpy(sums + sh, tmp);
+		sh+=n;
+		//Q_strcat( sums, sizeof( sums), va("%i ", chs ) ); // this is fugly
+	}
+	if (sh && sums[sh - 1]==' ') sums[sh - 1] = 0; // kill ending space
+
+	// unpack pk3 filenames
+	CL_TextDecode6Bit(buf + 2 + l, msg.cursize - (2 + l), names, sizeof(names));
+
+	// do simple integrity check, both should have same number of items
+	sh = 1; // number of spaces = number of names-1
+	i = 0;
+	while(names[i]) { if (names[i]==' ') sh++; i++; }
+	// fprintf(stderr,"FNAMES: %d  CHSUMS: %d\n",sh,l/4);
+
+	if (sh != l / 4)
+		Com_Printf("WARNING: Compressed purelist inconsistency!! (FN:%d/CH:%d)\n",sh,l/4);
+
+	// fprintf(stderr,"FNAMES: \"%s\"\n",names);
+	// fprintf(stderr,"CHSUMS: \"%s\"\n",sums);
+	FS_PureServerSetLoadedPaks( sums, names );
+
+}
+
+/*
 ==================
 CL_SystemInfoChanged
 
@@ -377,9 +560,15 @@ void CL_SystemInfoChanged( void ) {
 	}
 
 	// check pure server string
-	s = Info_ValueForKey( systemInfo, "sv_paks" );
-	t = Info_ValueForKey( systemInfo, "sv_pakNames" );
-	FS_PureServerSetLoadedPaks( s, t );
+	s = Info_ValueForKey(systemInfo, "sv_paks");
+	t = Info_ValueForKey(systemInfo, "sv_pakNames");
+	if (s[0] == '*' && s[1] == 0 && t[0] == '*' && t[1] == 0) {
+		Com_Printf("Using compressed pure file list\n");
+		CL_ParseCompressedPureList();
+	} else {
+		Com_Printf("Using default pure file list\n");
+		FS_PureServerSetLoadedPaks( s, t );
+	}
 
 	s = Info_ValueForKey( systemInfo, "sv_referencedPaks" );
 	t = Info_ValueForKey( systemInfo, "sv_referencedPakNames" );

code/qcommon/files.c
@@ -3519,6 +3519,34 @@ static void FS_Startup( const char *gameName )
 
 /*
 =====================
+FS_LoadedPakChecksumsBlob
+
+Return pak checksums as a binary blob with length at start
+=====================
+*/
+int FS_LoadedPakChecksumsBlob( unsigned char *dst, int dstlen ) {
+	searchpath_t    *search;
+	int num = 0;
+	int dp = 0;
+
+	for ( search = fs_searchpaths ; search ; search = search->next ) {
+		// is the element a pak file?
+		if ( !search->pack ) {
+			continue;
+		}
+		if (dp+4>=dstlen) return 0;
+		dst[dp+0] = search->pack->checksum&0xFF;
+		dst[dp+1] = (search->pack->checksum>>8)&0xFF;
+		dst[dp+2] = (search->pack->checksum>>16)&0xFF;
+		dst[dp+3] = (search->pack->checksum>>24)&0xFF;
+		dp+=4;
+		num++;
+	}
+	return dp;
+}
+
+/*
+=====================
 FS_LoadedPakChecksums
 
 Returns a space separated string containing the checksums of all loaded pk3 files.
@@ -3552,7 +3580,7 @@ Servers with sv_pure set will get this string and pass it to clients.
 =====================
 */
 const char *FS_LoadedPakNames( void ) {
-	static char	info[BIG_INFO_STRING];
+	static char	info[BIG_INFO_STRING*4];
 	searchpath_t	*search;
 
 	info[0] = 0;

code/qcommon/qcommon.h
@@ -710,6 +710,7 @@ const char *FS_LoadedPakChecksums( void );
 const char *FS_LoadedPakPureChecksums( void );
 // Returns a space separated string containing the checksums of all loaded pk3 files.
 // Servers with sv_pure set will get this string and pass it to clients.
+int FS_LoadedPakChecksumsBlob( unsigned char *dst, int dstlen );
 
 void FS_SetMapName( const char *mapname );
 void FS_SetExtraPure( const char *mapname, const char *extrapaks );
@@ -1223,5 +1224,11 @@ extern huffman_t clientHuffTables;
 #define DLF_NO_REDIRECT 2
 #define DLF_NO_UDP 4
 #define DLF_NO_DISCONNECT 8
+
+// compressed pure list buffer
+#define PURE_COMPRESS_BUFFER 16384
+
+// last N CS will be used:
+#define PURE_COMPRESS_NUMCS 8
 
 #endif // _QCOMMON_H_

code/server/server.h
@@ -303,6 +303,7 @@ extern	cvar_t	*sv_gametype;
 extern	cvar_t	*sv_pure;
 extern	cvar_t	*sv_extraPure;
 extern	cvar_t	*sv_extraPaks;
+extern	cvar_t	*sv_newpurelist;
 extern	cvar_t	*sv_floodProtect;
 extern	cvar_t	*sv_lanForceRate;
 extern	cvar_t	*sv_clientsPerIp;

code/server/sv_main.c
@@ -57,6 +57,7 @@ cvar_t	*sv_pure;
 cvar_t	*sv_extraPure;
 cvar_t	*sv_extraPaks;
 cvar_t	*sv_floodProtect;
+cvar_t	*sv_newpurelist;
 cvar_t	*sv_lanForceRate; // dedicated 1 (LAN) server forces local client rates to 99999 (bug #491)
 cvar_t	*sv_banFile;
 cvar_t	*sv_clientsPerIp;

commit 56aa302365f393da6678e55cb5724fcb6f0b66c5

Fix sv_extraPure (broken in 212af563)

code/server/sv_init.c
@@ -762,6 +762,20 @@ void SV_SpawnServer( char *server, qboolean killBots ) {
 	if ( sv_pure->integer ) {
 		// if a dedicated pure server we need to touch the cgame because it could be in a
 		// separate pk3 file and the client will need to load the latest cgame.qvm
+		if ( com_dedicated->integer ) {
+			SV_TouchCGame();
+		}
+
+		// Update latched value
+		Cvar_Get ("sv_extraPaks", "", CVAR_ARCHIVE | CVAR_LATCH);
+		Cvar_Get ("sv_extraPure", "0", CVAR_ARCHIVE | CVAR_LATCH);
+
+		if ( sv_extraPure->integer ) {
+			FS_SetExtraPure(server, sv_extraPaks->string);
+		}
+
+		// the server sends these to the clients so they will only
+		// load pk3s also loaded at the server
 		if (sv_newpurelist->integer) {
 			Cvar_Set( "sv_paks", "*" );
 			Cvar_Set( "sv_pakNames", "*" );
@@ -775,19 +789,6 @@ void SV_SpawnServer( char *server, qboolean killBots ) {
 			p = FS_LoadedPakNames();
 			Cvar_Set( "sv_pakNames", p );
 		}
-
-		if ( com_dedicated->integer ) {
-			SV_TouchCGame();
-		}
-
-		// Update latched value
-		Cvar_Get ("sv_extraPaks", "", CVAR_ARCHIVE | CVAR_LATCH);
-		Cvar_Get ("sv_extraPure", "0", CVAR_ARCHIVE | CVAR_LATCH);
-
-		if ( sv_extraPure->integer ) {
-			FS_SetExtraPure(server, sv_extraPaks->string);
-		}
-
 	}
 	else {
 		Cvar_Set( "sv_paks", "" );